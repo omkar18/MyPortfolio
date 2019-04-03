32c9343cb7c8d248be8b1841aa9fb090a4e4f2ab
Sobhit Kumar <sobhit2010@gmail.com>
2019-04-03 17:14:56 +0530

    navigation bar and route links done

---
+++ src/components/resume.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+
+
+class Resume extends Component {
+  render(){
+    return(
+      <div><h1>Resume Works</h1></div>
+    )
+  }
+}
+
+export default Resume;
